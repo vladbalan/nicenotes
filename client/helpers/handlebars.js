06687cbfe99d762515ab7be2a42e724e14dd5f6c
vladbalan <vlad.balan.dev@gmail.com>
2015-03-14 17:50:17 +0200

    Improve app design

--- client/helpers/handlebars.js
+++ client/helpers/handlebars.js
@@ -5,3 +5,14 @@ Handlebars.registerHelper('errorMessage', function(field, templateName) {
 Handlebars.registerHelper('errorClass', function(field, templateName) {
 	return !!Session.get(templateName + 'Validator')[field] ? 'has-error' : '';
 });
+
+Handlebars.registerHelper('activeRouteClass', function(/* route names */) {
+    var args = Array.prototype.slice.call(arguments, 0);
+    args.pop();
+    
+    var active = _.any(args, function(name) {
+      return Router.current() && Router.current().route.getName() === name
+    });
+    
+    return active && 'active';
+});
--- client/views/template/layout.js
+++ client/views/template/layout.js
@@ -1,5 +1,6 @@
 Template.layout.rendered = function() {
 	$('[data-toggle=offcanvas]').click(function() {
 		$('.row-offcanvas').toggleClass('active');
+		$('.btn-toggle-sidebar').toggleClass('glyphicon-chevron-left').toggleClass('glyphicon-chevron-right')
 	});
 }
